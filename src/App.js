912fc8bb90f54fe51c81c2fb57a46c496e6ec3d7
kaka <1393597501@qq.com>
2019-06-21 23:18:39 +0800

    使用ui组件演示成功

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Button,WhiteSpace} from 'antd-mobile';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-       移动端项目
+      <Button>default</Button><WhiteSpace />
+      <Button disabled>default disabled</Button><WhiteSpace />
+
+      <Button type="primary">primary</Button><WhiteSpace />
+      <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
+
+      <Button type="warning">warning</Button><WhiteSpace />
+      <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
     </div>
   );
 }
